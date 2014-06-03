20d160c0a6491ba40ad22dfac6a6db660e30885b
Aruna321 <sekira19@gmail.com>
2014-06-03 04:18:11 -0700

    prototype

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// My Scripts
+$(document).foundation({
+	orbit: {
+		timer_show_progress_bar: false,
+		timer: false,
+		navigation_arrows: false,
+		slide_number: false,
+		bullets: false,
+		next_on_click: false
+
+
+	}
+});
+
+
